8054f9339f88fd2ab30ee88ade42a452ec4ad11c
Ivan Pons Oltra <llimapons@hotmail.com>
2026-03-05 17:03:03 +0100

    Add convention plugins

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,6 +11,10 @@ dependencies {
     compileOnly(libs.android.tools.common)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.androidx.room.gradle.plugin)
+    implementation(libs.buildkonfig.gradlePlugin)
+    implementation(libs.buildkonfig.compiler)
 }
 
 java {
@@ -21,5 +25,49 @@ java {
 kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "com.llimapons.convention.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidComposeApplication") {
+            id = "com.llimapons.convention.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("cmpApplication") {
+            id = "com.llimapons.convention.cmp.application"
+            implementationClass = "CmpApplicationConventionPlugin"
+        }
+        register("kmpLibrary") {
+            id = "com.llimapons.convention.kmp.library"
+            implementationClass = "KmpLibraryConventionPlugin"
+        }
+        register("cmpLibrary") {
+            id = "com.llimapons.convention.cmp.library"
+            implementationClass = "CmpLibraryConventionPlugin"
+        }
+        register("cmpFeature") {
+            id = "com.llimapons.convention.cmp.feature"
+            implementationClass = "CmpFeatureConventionPlugin"
+        }
+        register("buildKonfig") {
+            id = "com.llimapons.convention.buildkonfig"
+            implementationClass = "BuildKonfigConventionPlugin"
+        }
+        register("room") {
+            id = "com.llimapons.convention.room"
+            implementationClass = "RoomConventionPlugin"
+        }
     }
 }
---
+++ build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
@@ -0,0 +1,27 @@
+import com.llimapons.echo.convention.configureAndroidTarget
+import com.llimapons.echo.convention.configureIosTargets
+import com.llimapons.echo.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class CmpApplicationConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target){
+            with(pluginManager){
+                apply("com.llimapons.convention.android.application.compose")
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("org.jetbrains.compose")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            configureAndroidTarget()
+            configureIosTargets()
+
+            dependencies{
+                "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+            }
+        }
+    }
+}
